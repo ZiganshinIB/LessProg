b7a6d0d414b9271bfd70ff4db5c38ced15c52d00
ZiganshinIB <elzig3012.gmail.com>
2022-12-03 09:51:40 +0300

    Upgrade

--- Program.cs
+++ Program.cs
@@ -1,14 +1,30 @@
 ﻿Console.Clear();
-Console.Write("Введите число a: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число b: ");
-int b = Convert.ToInt32(Console.ReadLine());
+int taskNomber = 42;
+Console.WriteLine("Выбире задание \n В случае когда вы хотите завершить введите 0");
 
-if (a > b){
-    Console.WriteLine($"max = {a}");
-    Console.WriteLine($"min = {b}");
-}else if (a < b){
-    Console.WriteLine($"max = {b}");
-    Console.WriteLine($"min = {a}");
-}else    Console.WriteLine($"{a} = {b}");
+while (taskNomber != 0){
+    taskNomber = Convert.ToInt32(Console.ReadLine());
 
+    switch(taskNomber){
+        case 1: 
+            Console.Write("Введите число a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b){
+                Console.WriteLine($"max = {a}");
+                Console.WriteLine($"min = {b}");
+            }else if (a < b){
+                Console.WriteLine($"max = {b}");
+                Console.WriteLine($"min = {a}");
+            }else    Console.WriteLine($"{a} = {b}");
+            break;
+        case 2: break;
+        case 3: break;
+        case 4: break;
+        default: 
+            Console.WriteLine("Ответ: 42");
+            break;
+}
+}
