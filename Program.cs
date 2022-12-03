495425dffe56c387051bac83094d447a28265f86
ZiganshinIB <elzig3012.gmail.com>
2022-12-03 09:28:20 +0300

    Start

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b){
+    Console.WriteLine($"max = {a}");
+    Console.WriteLine($"min = {b}");
+}else if (a < b){
+    Console.WriteLine($"max = {b}");
+    Console.WriteLine($"min = {a}");
+}else    Console.WriteLine($"{a} = {b}");
+
