183d91ceeeee68de6f1ca64399d1738515f5b938
ZiganshinIB <elzig3012.gmail.com>
2022-12-04 15:01:49 +0300

    Add task21

--- Program.cs
+++ Program.cs
@@ -134,7 +134,23 @@ while (taskNomber != 0){
                     Console.WriteLine("Alarm");
                     break;        
             }
-            break; 
+            break;
+        case 21:
+            Console.Write("Введите координат x 1-ой точки: ");
+            double x_1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координат y 1-ой точки: ");
+            double y_1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координат x 2-ой точки: ");
+            double x_2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координат y 2-ой точки: ");
+            double y_2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(Math.Round(
+            Math.Sqrt(
+            Math.Pow(x_1-x_2,2)+
+                Math.Pow(y_1-y_2,2)
+                ),2));
+            break;
         default: 
             Console.WriteLine("Ответ: 42");
             break; 
